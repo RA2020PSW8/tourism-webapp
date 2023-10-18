a1e9c25fd9bc93e6c70ea73c6ebb110b17702ba0
milica <milicakljajic1@gmail.com>
2023-10-18 22:13:17 +0200

    style: redesigned tour issue components

--- Explorer/src/app/feature-modules/tour-execution/tour-execution.module.ts
+++ Explorer/src/app/feature-modules/tour-execution/tour-execution.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TourIssueComponent } from './tour-issue/tour-issue.component';
 import { TourIssueFormComponent } from './tour-issue-form/tour-issue-form.component';
+import { MaterialModule } from 'src/app/infrastructure/material/material.module';
 import { ReactiveFormsModule } from '@angular/forms';
 
 
@@ -13,6 +14,7 @@ import { ReactiveFormsModule } from '@angular/forms';
   ],
   imports: [
     CommonModule,
+    MaterialModule,
     ReactiveFormsModule
   ]
 })
