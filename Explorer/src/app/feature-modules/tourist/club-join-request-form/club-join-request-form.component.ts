74a51f15d0f098e160043c3749f310d321eed877
MiljanaMa <miljana.marjanovic9@gmail.com>
2023-10-22 14:35:19 +0200

    feat: add request input form

--- Explorer/src/app/feature-modules/tourist/club-join-request-form/club-join-request-form.component.ts
+++ Explorer/src/app/feature-modules/tourist/club-join-request-form/club-join-request-form.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { TouristService } from '../tourist.service';
+import { ClubJoinRequest } from '../model/club-join-request.model';
 
 @Component({
   selector: 'xp-club-join-request-form',
@@ -14,5 +15,22 @@ export class ClubJoinRequestFormComponent {
     this.clubJoinRequestForm = new FormGroup({
       id: new FormControl('', [Validators.required]),
     });
+  }
+  sendRequest(): void {
+    const clubIdToSend = this.clubJoinRequestForm.value.clubId || 0;
+
+    /*const request: ClubJoinRequest = {
+      clubId: this.club.id || 0,
+      userId: userIdToAdd
+    }
+
+    this.service.addClubInvitation(clubInvitation).subscribe({
+      next: (_) => {
+        this.clubInvitationsUpdated.emit()
+      }
+    });
+
+    //auto add without accepting invitations - JUST TEMPORARY
+    this.service.updateClub(this.club).subscribe({});*/
   }
 }
