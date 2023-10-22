d9c3499c4aab701517c54e799894bb32a172e2e6
Anastasija Novakovic <anastasijanov2323@gmail.com>
2023-10-22 17:36:04 +0200

    feat: add component

---
+++ Explorer/src/app/feature-modules/administration/app-rating/app-rating.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'xp-app-rating',
+  templateUrl: './app-rating.component.html',
+  styleUrls: ['./app-rating.component.css']
+})
+export class AppRatingComponent {
+
+}
