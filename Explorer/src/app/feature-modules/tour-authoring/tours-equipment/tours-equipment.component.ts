64b17514d15c5ea11974816cecf3734e627545cf
Srđan Petronijević <srdjanpetronijevic@outlook.com>
2023-10-25 21:37:42 +0200

    fix: ensure tour exists and has id in loadEquipmentData method

--- Explorer/src/app/feature-modules/tour-authoring/tours-equipment/tours-equipment.component.ts
+++ Explorer/src/app/feature-modules/tour-authoring/tours-equipment/tours-equipment.component.ts
@@ -31,11 +31,12 @@ export class ToursEquipmentComponent implements OnInit {
   loadEquipmentData() {
     this.tourAuthoringService.getEquipment().subscribe(pagedResults => {
       this.allEquipment = pagedResults.results;
+      if (this.tour && this.tour.id) {
       this.tourAuthoringService.getEquipmentForTour(this.tour.id).subscribe(pagedResults => {
         this.selectedEquipment = pagedResults;
         console.log(this.selectedEquipment);
         this.availableEquipment = this.allEquipment.filter(e => !this.selectedEquipment.some(se => se.id === e.id));
-        
+        }
       });
       console.log(this.allEquipment);
     });
