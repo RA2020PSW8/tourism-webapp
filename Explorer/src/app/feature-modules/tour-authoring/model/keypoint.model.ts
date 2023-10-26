d7d6f9ce3ffb042c1775cf0f8f75eba72e462c99
Nikola Simić <simic.ra32.2020@uns.ac.rs>
2023-10-26 15:26:24 +0200

    Revert "feat: blog comments (#17)"
    This reverts commit 3efbb367ebaf0af83ed5c13a61d5fc63fcf2d4f0.

--- Explorer/src/app/feature-modules/tour-authoring/model/keypoint.model.ts
+++
@@ -1,7 +0,0 @@
-export interface Keypoint {
-    id?: number,
-    name: string,
-    latitude: number,
-    longitude: number,
-    description?: string
-}
--- Explorer/src/app/feature-modules/tour-authoring/tour-authoring.module.ts
+++ Explorer/src/app/feature-modules/tour-authoring/tour-authoring.module.ts
@@ -1,22 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { KeypointComponent } from './keypoint/keypoint.component';
-import { MaterialModule } from 'src/app/infrastructure/material/material.module';
-import { MatTable, MatTableModule } from '@angular/material/table';
-import { KeypointFormComponent } from './keypoint-form/keypoint-form.component';
-import { ReactiveFormsModule } from '@angular/forms';
 
 
 
 @NgModule({
-  declarations: [
-    KeypointComponent,
-    KeypointFormComponent
-  ],
+  declarations: [],
   imports: [
-    CommonModule,
-    MaterialModule,
-    ReactiveFormsModule
+    CommonModule
   ]
 })
 export class TourAuthoringModule { }
