70ab45e9c4b9bc6141bc6c5e34bf7495bd76b028
Nikola Simić <nikola7simic@gmail.com>
2023-10-26 17:31:43 +0200

    feat: blog comments (#26)
    * feat: implemented blog commenting crud
    
    * feat: implemented crud for blog comments
    
    * fix: added a comma
    
    * fix: environment conflict
    
    ---------
    
    Co-authored-by: Mihailo <zupermancile@gmail.com>

---
+++ Explorer/src/app/feature-modules/blog/comment-form/comment-form.component.ts
@@ -0,0 +1,80 @@
+import { Component, EventEmitter,Input, OnChanges, Output, SimpleChanges } from '@angular/core';
+import { FormControl, FormGroup,Validators } from '@angular/forms';
+import { CommentService } from '../comment.service';
+import { Comment } from '../model/comment.model';
+
+@Component({
+  selector: 'xp-comment-form',
+  templateUrl: './comment-form.component.html',
+  styleUrls: ['./comment-form.component.css']
+})
+
+export class CommentFormComponent implements OnChanges {
+
+@Output() commentAdded = new EventEmitter<null>();
+@Input() comment : Comment;
+@Input() editMode: boolean = false;
+
+constructor(private service: CommentService) {}
+ 
+ngOnChanges(changes: SimpleChanges): void {
+  this.commentForm.reset();
+  if(this.editMode == true){
+    this.commentForm.patchValue(this.comment);
+  }
+  }
+
+  commentForm = new FormGroup({
+    comment: new FormControl('',[Validators.required]),
+  })
+
+  ngOnInit(){}
+
+  onSubmit():void{
+    if(this.commentForm.valid){
+      alert('top');
+    }else{
+      alert('bottom');
+    }
+  }
+
+  createComment(): void
+  {
+    const newComment: Comment = {
+      forumId: 1,
+      username: 'miki',
+      postTime: new Date(),
+      lastEditTime: new Date(),
+      comment: this.commentForm.value.comment || "", 
+      isDeleted: false
+    }
+    this.service.createComment(newComment).subscribe({
+      next: (_) => {
+        this.commentAdded.emit();
+      }
+    });
+  }
+
+  updateComment(): void
+  {
+    const com : Comment = {
+      comment: this.commentForm.value.comment || "",
+      forumId: 0,
+      username: '',
+      postTime: new Date(),
+      isDeleted: false
+    }
+    com.id = this.comment.id;
+    com.forumId = this.comment.forumId;
+    com.username = this.comment.username;
+    com.lastEditTime = this.comment.lastEditTime;
+    com.postTime = this.comment.postTime;
+    com.isDeleted = this.comment.isDeleted;
+
+    this.service.updateComment(com).subscribe({
+      next: (_) => {
+        this.commentAdded.emit();
+      }
+    });
+  }
+}
