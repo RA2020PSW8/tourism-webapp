f2e7001a703d800bd936c5d7671e97e32a7e1c73
milena <milenamarkovickg@gmail.com>
2023-11-11 15:40:14 +0100

    feat: add notifications box

--- Explorer/src/app/feature-modules/layout/navbar/navbar.component.ts
+++ Explorer/src/app/feature-modules/layout/navbar/navbar.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/infrastructure/auth/auth.service';
 import { User } from 'src/app/infrastructure/auth/model/user.model';
+import { LayoutService } from '../layout.service';
+import { PagedResults } from 'src/app/shared/model/paged-results.model';
+import { NotificationModel } from '../model/notification.model';
 
 @Component({
   selector: 'xp-navbar',
@@ -10,16 +13,51 @@ import { User } from 'src/app/infrastructure/auth/model/user.model';
 export class NavbarComponent implements OnInit {
 
   user: User | undefined;
+  notifications: NotificationModel[] = [];
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private layoutService: LayoutService) {}
 
   ngOnInit(): void {
     this.authService.user$.subscribe(user => {
       this.user = user;
     });
+    this.getNotifications();
   }
 
   onLogout(): void {
     this.authService.logout();
+  }
+
+  getNotifications(): void {
+    this.layoutService.getNotifications().subscribe({
+      next: (result: PagedResults<NotificationModel>) => {
+        this.notifications = result.results;
+      },
+      error: (err: any) => {
+        console.log(err);
+      }
+    })
+  }
+
+  markAsRead(notification: NotificationModel): void {
+    this.layoutService.markAsRead(notification).subscribe({
+      next: () => {
+        this.getNotifications();
+      },
+      error: (err: any) => {
+        console.log(err);
+      }
+    })
+  }
+
+  delete(notificationId: number): void {
+    this.layoutService.delete(notificationId).subscribe({
+      next: () => {
+        this.getNotifications();
+      },
+      error: (err: any) => {
+        console.log(err);
+      }
+    })
   }
 }
