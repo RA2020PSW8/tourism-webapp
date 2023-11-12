0b56bec8ba5f22301c7d3f315eca87979acd9da5
Nikola Simić <nikola7simic@gmail.com>
2023-11-12 18:53:21 +0100

    feat: chat (#49)
    * feat: show chat previews
    
    * feat: display messages
    
    * feat: send messages from chat
    
    * chore: ajust preview message wrap
    
    * fix: rebuild package-lock.json ones and for all
    
    * feat: start chat with any follower
    
    * feat: add unread sign for messages
    
    ---------
    
    Co-authored-by: MiljanaMa <miljana.marjanovic9@gmail.com>

---
+++ Explorer/src/app/feature-modules/administration/chat-messages-view/chat-messages-view.component.ts
@@ -0,0 +1,46 @@
+import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
+import { ProfileService } from '../profile.service';
+import { ChatMessage } from '../model/chat-preview.model';
+import { MessageInput } from '../model/message-input.model';
+
+@Component({
+  selector: 'xp-chat-messages-view',
+  templateUrl: './chat-messages-view.component.html',
+  styleUrls: ['./chat-messages-view.component.css']
+})
+export class ChatMessagesViewComponent implements OnChanges {
+
+  @Input() followerId: number;
+  @Output() messageSent = new EventEmitter<void>();
+
+  public messages: ChatMessage[] = [];
+  public messageContent: string;
+
+  constructor(private profileService: ProfileService){}
+
+  ngOnChanges(): void {
+    if(this.followerId > 0){
+      this.getMessages();
+    }
+  }
+
+  getMessages(): void{
+    this.profileService.getMessages(this.followerId).subscribe(res => {
+      this.messages = res.results;
+      this.messageSent.emit();
+    });    
+  }
+
+  sendMessage(){
+    if(this.messageContent){
+      let message: MessageInput = {
+        receiverId: this.followerId,
+        content: this.messageContent
+      }
+      this.profileService.sendMessage(message).subscribe(res => {
+        this.messageContent = '';
+        this.getMessages();
+      });
+    }
+  }
+}
