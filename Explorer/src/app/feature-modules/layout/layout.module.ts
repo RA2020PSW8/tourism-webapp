437fd77af2fe909d0b3aee5d43ddd21a1580ece4
Srđan Petronijević <srdjanpetronijevic@outlook.com>
2023-11-14 16:52:11 +0100

    feat: limited view of tours (#51) + calculate average rate
    * feat: limited view of tours
    - Add tour-card-compact component that will represent the tour card on home.
    - Add a way to check if a user is logged in or not.
    - Remove unnecessary, unused CSS selectors in home.component.css.
    - Add a way for users to see tour reviews and to add tours to the shopping cart.
    - Bonus: Lost my sanity.
    
    * feat: limited view of tours
    - Add tour-card-compact component that will represent the tour card on home.
    - Add a way to check if a user is logged in or not.
    - Remove unnecessary, unused CSS selectors in home.component.css.
    - Add a way for users to see tour reviews and to add tours to the shopping cart.
    - Bonus: Lost my sanity.
    
    * fix: Added parseToInt for data consistency.
    
    * feat: add display of purchased tours
    
    * feat: calculate average rate (#55)
    
    Co-authored-by: Nikola Simić <nikola7simic@gmail.com>
    
    ---------
    
    Co-authored-by: Anastasija Radic <116906142+anastasijaradic@users.noreply.github.com>
    Co-authored-by: Nikola Simić <nikola7simic@gmail.com>

--- Explorer/src/app/feature-modules/layout/layout.module.ts
+++ Explorer/src/app/feature-modules/layout/layout.module.ts
@@ -6,29 +6,30 @@ import { MaterialModule } from 'src/app/infrastructure/material/material.module'
 import { RouterModule } from '@angular/router';
 import { ProfileComponent } from '../administration/profile/profile.component';
 import { AdministrationModule } from '../administration/administration.module';
-import {MatGridListModule} from '@angular/material/grid-list';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MarketplaceModule } from "../marketplace/marketplace.module";
 import { MatMenuModule } from '@angular/material/menu';
 
 @NgModule({
   declarations: [
     HomeComponent,
     NavbarComponent,
-    
+
   ],
   imports: [
     CommonModule,
     MaterialModule,
-    MatMenuModule,
     RouterModule,
     AdministrationModule,
     MatGridListModule,
-
+    MarketplaceModule,
+    MatMenuModule,
   ],
   exports: [
     NavbarComponent,
-    HomeComponent, 
+    HomeComponent,
     AdministrationModule,
-    MatGridListModule
+    MatGridListModule,
   ]
 })
 export class LayoutModule { }
