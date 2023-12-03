531db24bf54935da18b02ecac065740c96d90487
Anastasija Novakovic <anastasijanov2323@gmail.com>
2023-12-03 01:58:33 +0100

    remove comments

--- Explorer/src/app/feature-modules/marketplace/shopping-cart-overview/shopping-cart-overview.component.ts
+++ Explorer/src/app/feature-modules/marketplace/shopping-cart-overview/shopping-cart-overview.component.ts
@@ -51,17 +51,15 @@ export class ShoppingCartOverviewComponent implements OnInit {
   calculateTotalPrice(): number {
     this.totalPrice = this.orders.reduce((total, order) => total + order.tourPrice, 0);
     return this.totalPrice;
-    //return this.orders.reduce((total, order) => total + order.tourPrice, 0);
-
   }
   checkout() : void{
-    this.getOrders(); // to update price if some items are removed
+    this.getOrders(); 
     this.calculateTotalPrice(); 
     if(this.wallet.adventureCoins < this.totalPrice){
       console.log(this.wallet);
       console.log('ukupna cena: ' ,this.totalPrice);
       alert('Error. You dont have enough ACs(adventure coins) in your wallet.');
-    } else{  //proveri da li se ovo izvrsava kako treba, u dobrom redosledju jer iako nema dovoljno para u wallwtu, on ispise error za vec kupljenen ture iz else dela
+    } else{  
       
       if(window.confirm('Are you sure that you want to purchase these tours?')){
         this.marketplaceService.buyShoppingCart(Number(this.shoppingCart.id)).subscribe({  
@@ -74,47 +72,9 @@ export class ShoppingCartOverviewComponent implements OnInit {
             alert('Error. Some tours were already purchased.');
           }
         })
-        /*this.wallet.adventureCoins = this.wallet.adventureCoins - this.totalPrice;
-        this.marketplaceService.updateWallet(this.wallet).subscribe({
-          next:(result:Wallet) => {
-            console.log(result);
-          },
-          error:(err:any) => {
-            console.log(err);
-          }
-        })*/
-
       }
     }
   }
-  
-  /*checkout(): void {
-    if (window.confirm('Are you sure that you want to purchase these tours?')) {
-      if (this.wallet.adventureCoins < this.totalPrice) {
-        console.log(this.wallet);
-        console.log('ukupna cena: ', this.totalPrice);
-        alert('Error. You dont have enough ACs(adventure coins) in your wallet.');
-      } else {
-        this.marketplaceService.buyShoppingCart(Number(this.shoppingCart.id)) .pipe(
-          mergeMap(() => {
-            this.wallet.adventureCoins = this.wallet.adventureCoins - this.totalPrice;
-            return this.marketplaceService.updateWallet(this.wallet);
-          })
-        ).subscribe({
-          next: (result: Wallet) => {
-            console.log(result);
-            window.alert('Successfully purchased');
-            this.getOrders();
-          },
-          error: (err: any) => {
-            console.log(err);
-            alert('Error. Some tours were already purchased.');
-          }
-        });
-      }
-    }
-  }*/
-  
 
   getWallet(): void {
     this.marketplaceService.getWalletForUser().subscribe({
