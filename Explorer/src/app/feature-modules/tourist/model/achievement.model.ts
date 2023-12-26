4d72c65353652c49edd47b0fb0c896a1dac2b283
MiljanaMa <miljana.marjanovic9@gmail.com>
2023-12-26 18:04:00 +0100

    feat: add achievements into club detail page

--- Explorer/src/app/feature-modules/tourist/model/achievement.model.ts
+++ Explorer/src/app/feature-modules/tourist/model/achievement.model.ts
@@ -2,5 +2,12 @@ export interface Achievement {
     id?: number,
     name: string,
     description: string,
-    type: number,
+    type: AchievementType,
+}
+export enum AchievementType {
+    HIDDEN_ENCOUNTER = "HIDDEN_ENCOUNTER",
+    FIGTH_1 = "FIGTH_1",
+    FIGTH_2 = "FIGTH_2",
+    SOCIAL = "SOCIAL"
+    
 }
